eec6d11fa5af9011ab2fe4eea8410406c07739e9
CourtneyWB <courtney@webill.net>
2021-05-20 13:23:30 +0200

    commit form changes

--- src/Components/FormFolder/MainForm.js
+++ src/Components/FormFolder/MainForm.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React,{Component,useState} from "react";
 import Modal from "react-modal";
-import { Formik, Form, ErrorMessage } from "formik";
+import { Formik, Form, FormikProps,Field,ErrorMessage } from "formik";
 import * as Yup from "yup";
+// import Dropdown from '@bit/react-bootstrap.react-bootstrap.dropdown';
+// import ReactBootstrapStyle from '@bit/react-bootstrap.react-bootstrap.internal.style-links';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { TextField } from "./TextField";
 
+
 const MainForm = () => {
 
-  const validate = Yup.object({
+const validate = Yup.object({
 
     firstName: Yup.string()
       .max(10, "Must be 15 characters or less")
@@ -22,6 +25,8 @@ const MainForm = () => {
     .required("Required"),
 
   });
+// class DropDownMenu extends Component {
+//     render() {
   return (
     <Formik
       initialValues={{
@@ -37,9 +42,27 @@ const MainForm = () => {
           <Form>
             <TextField label="First Name" name="firstName" type="text" />
             <TextField label="Last Name" name="lastName" type="text" />
-            <TextField label="Email" name="Email" type="email" />
-            <TextField label="Favourite Color" name="favouriteColor" type="text" />
-           
+            <TextField label="Email" name="email" type="email" /><br></br>
+            <Field style={{width:"81em",height:"2.3em",borderColor:"lightGrey",borderRadius: "6px"}} as="select" label= "favouriteColor" name="favouriteColor">
+      
+            <option value="red">Choose...</option>
+             <option value="red">Red</option>
+             <option value="green">Green</option>
+             <option value="blue">Blue</option>
+           </Field>
+            {/* <Dropdown>
+				<Dropdown.Toggle variant="white" id="dropdown-basic">
+				Choose...
+  </Dropdown.Toggle>
+
+				<Dropdown.Menu>
+					<Dropdown.Item href="#/action-1">red</Dropdown.Item>
+					<Dropdown.Item href="#/action-2">blue</Dropdown.Item>
+					<Dropdown.Item href="#/action-3">green</Dropdown.Item>
+				</Dropdown.Menu>
+			</Dropdown> */}
+		{/* ) */}
+<br></br>           
             <button style={{background:"blue",border:"blue",
             paddingRight: "15px"}} 
             className ="btn btn-dark mt-3" 
@@ -47,12 +70,13 @@ const MainForm = () => {
             Position = "center"
             type="submit">Submit form
             </button>
-            
+
           </Form>
         </div>
+        
       {/* )} */}
     </Formik>
   );
-};
+}
 
 export default MainForm;
